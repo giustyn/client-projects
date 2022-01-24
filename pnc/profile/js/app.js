d95dac3a27b7cc2cada2aefb32251ac0801b6b8d
giustyn <80340879+giustyn@users.noreply.github.com>
2022-01-24 01:23:25 -0500

    commit

--- pnc/profile/js/app.js
+++ pnc/profile/js/app.js
@@ -1,7 +1,6 @@
-$(function () {
-    var example = [{
-        // filename: "https://photos-dev.adrenalineamp.com/data/73025ae3-a51e-400c-8608-020bf70cc182.jpg",
-        filename: "img/photo-1568493021943-4077b55c95a3.jpg",
+(function () {
+    var example = {
+        pahtname: "./img/photo-1568493021943-4077b55c95a3.jpg",
         id: 11,
         jobTitle: {
             title: "Branch Manager",
@@ -37,12 +36,15 @@ $(function () {
             displayName: "Kylo",
             id: 23
         }
-    }];
+    };
 
     function getStaff(onSuccess, onError) {
+        var tag = window.parent.PlayerSDK.getTagByPrefix("Z.MPS.PNC.");
+        var templateGroup = "pnc_2021_staff"; // Note: "pnc_2021_staff_mlo" for mlo templates
+
         return $.ajax({
             method: "GET",
-            url: "https://photos-dev.adrenalineamp.com/public-api/mps/_MPS.PNC.TEST-PLAYER/staff?a=e85711db-6395-4811-94c4-93ec1e83f4b3",
+            url: "https://photos-dev.adrenalineamp.com/public-api/mps/" + tag.Name + "/template/" + templateGroup + "?a=e85711db-6395-4811-94c4-93ec1e83f4b3",
             dataType: 'json',
             success: function (result) {
                 console.log(result)
@@ -63,18 +65,16 @@ $(function () {
                 loop: false,
                 autoplay: false,
                 duration: speed,
-                easing: 'easeInOutExpo',
+                easing: 'easeInOutQuart',
             })
             .add({
                 targets: '.container',
-                easing: 'easeInOutSine',
-                delay: speed,
                 opacity: [0, 1],
-            }, '-=' + (speed) + '')
+            })
             .add({
                 targets: '.heading',
                 translateY: '100%',
-                translateX: ['100%', '0%'],
+                translateX: ['200%', '0%'],
                 opacity: [0, 1],
             })
             .add({
@@ -87,8 +87,8 @@ $(function () {
                 translateX: ['100%', '0%'],
                 opacity: [0, 1],
                 scale: [0, 1],
-                duration: (speed * 2),
-            }, '-=' + (speed * 3.5) + '')
+                // duration: (speed * 2),
+            }, '-=' + (speed * 3) + '')
             /** animate card #1 */
             .add({
                 targets: '.card-1, .card-1 *',
@@ -138,67 +138,62 @@ $(function () {
             }, '-=' + (speed / 2) + '')
             .add({
                 targets: '#template',
+                easing: 'easeInOutSine',
                 opacity: [1, 0],
-            }, '-=' + (speed / 2) + '')
+            })
 
         animation.play();
     }
 
-    function assembleTeam(response) {
-        $(response).each((i => {
-            var photo = response[i];
+    function assembleTeam(photo) {
 
-            var img = new Image();
-
-            img.onerror = function () {
-                $('.heading').html("no img!");
+        /**
+         * This array check should no longer be needed. API endpoint now sends a single json object
+         */
+        if (Array.isArray(photo)) {
+            if (!photo.length) {
+                onTemplateError();
             }
+            photo = photo.pop();
+        }
 
-            img.src = photo.filename;
+        var img = new Image();
 
+        img.onload = function () {
             $('.name').html(photo.user.displayName);
             $('.title').html(photo.jobTitle.title);
             $('.heading').html(photo.jobTitle.templateValues.heading.value);
             $('.message').html(photo.jobTitle.templateValues.message.value);
             $('.legal').html(photo.jobTitle.templateValues.legal.value);
-            $('.img').attr('src', photo.filename);
+            $('.img').attr('src', photo.pathname).show();
             animate();
-        }));
+        }
+
+        img.onerror = function () {
+            onTemplateError();
+        }
+
+        img.src = photo.pathname;
+    }
+
+    // TODO: Handle Fallback Animation
+    function onTemplateError() {
+        assembleTeam(example);
     }
 
     function init() {
-        // getStaff(assembleTeam, console.log);
-
-        /* test example data  */
-        getTestData();
+        let bgVideo = $('#bgVideo').attr('src','./video/ceCOM_XS.mp4');
+        getStaff(assembleTeam, onTemplateError);
     }
 
-    init();
+    window.addEventListener("sdk-ready", function (e) {
+        /**
+         * e.detail contains the PlayerSDK object.
+         * It is also attached at window.parent.PlayerSDK
+         */
+        // console.log(e.detail);
 
-    function contingency() {
-        var source = "video/PNC5027_Mortgage_HLC_BED.mp4",
-            video = $('.background video'),
-            container = $('.container');
-
-        video.attr('src', source);
-        container.remove();
-    }
-
-    function getTestData(response) {
-
-        if (example.length === 0) return contingency();
-        
-        $(example).each((i => {
-            var data = example[i];
-            console.log(data);
-            $('.name').html(data.user.displayName);
-            $('.title').html(data.jobTitle.title);
-            $('.heading').html(data.jobTitle.templateValues.heading.value);
-            $('.message').html(data.jobTitle.templateValues.message.value);
-            $('.legal').html(data.jobTitle.templateValues.legal.value);
-            $('.img').attr('src', data.filename);
-            animate();
-        }));
-    }
+        init();
+    })
 
 })();
